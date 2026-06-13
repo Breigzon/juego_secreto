56df0b1c5b604bbbb6140d7c49f7ef846a1df557
Breigzon Turco <breigzonandre@gmail.com>
2026-06-13 16:42:02 -0500

    Cambios en js

---
+++ PROYECTOS GIT/NUMERO SECRETO/app.js
@@ -0,0 +1,18 @@
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    // Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p', 'Ya se sortearon todos los números posibles');
+    } else {
+        // Si el numero generado está incluido en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
